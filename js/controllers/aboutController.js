33a03072053fa938cfa8ee481b321ce26b63b624
Rafal <rafal.slusarczyk90@gmail.com>
2015-02-12 19:53:39 +0100

    next changes in project

--- js/controllers/aboutController.js
+++ js/controllers/aboutController.js
@@ -1,6 +1,5 @@
 angular.module("carApp").controller("myCtrl", function($scope, ItemsService) {
         
-        $scope.car = {};
         $scope.carsCategories = ItemsService.getCarsCategories();
         $scope.addCarCategory = function(name, cars) {
             ItemsService.addCarCategory({name: name, cars: cars});
@@ -15,4 +14,29 @@ angular.module("carApp").controller("myCtrl", function($scope, ItemsService) {
             ItemsService.updateCarCategory(id); 
         }
 
+        $scope.addCar = function(category, car) {
+            if(category.cars) {
+                category.cars.push(car);
+            } else {
+                category.cars = new Array();
+                category.cars.push(car);
+            }
+        }
+
+        $scope.showAddCarPanel = function(category)  {
+            if(category.addCarPanelAvailable) {
+                category.addCarPanelAvailable = false;
+            } else {
+                var carsCategories = ItemsService.getCarsCategories();
+                carsCategories.forEach(function(cat) {
+                    if(cat.addCarPanelAvailable) {
+                        cat.addCarPanelAvailable = false;    
+                        ItemsService.updateCarCategory(cat);
+                    }
+                });
+                category.addCarPanelAvailable = true;
+            }
+            ItemsService.updateCarCategory(category);
+        }
+ 
     })
