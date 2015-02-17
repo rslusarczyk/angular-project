fbb8c0bee2a13e7663304b25b1f3f023a3f96a41
Rafal <rafal.slusarczyk90@gmail.com>
2015-02-17 01:50:41 +0100

    changed names of files

---
+++ js/controllers/listController.js
@@ -0,0 +1,90 @@
+angular.module("carApp").controller("myCtrl", function($scope, ItemsService) {
+        $scope.carToAdd = {brand:"",model:"", year:"", engine:"",description:"",price:""};
+        $scope.carEdit = {id: -1000, carId: -1000};
+        $scope.carDetails = {id: -1000, carId: -1000};
+        $scope.carCategoryEdit = {id: -1000};
+        $scope.carsCategories = ItemsService.getCarsCategories();
+        $scope.addCarCategory = function(name, cars) {
+            ItemsService.addCarCategory({name: name, cars: cars});
+            
+        }
+        $scope.removeCarCategory = function(id) { 
+            ItemsService.removeCarCategory(id);
+        }
+        
+        $scope.updateCarCategory = function(id) { 
+            ItemsService.updateCarCategory(id); 
+        }
+
+        $scope.addCar = function(category, car) {
+            if(category.cars) {
+                category.cars.push(car);
+            } else {
+                category.cars = new Array();
+                category.cars.push(car);
+            }
+
+            $scope.carToAdd = {brand:"",model:"", year:"", engine:"",description:"",price:""};
+        }
+
+        $scope.showAddCarPanel = function(category)  {
+            if(category.addCarPanelAvailable) {
+                category.addCarPanelAvailable = false;
+            } else {
+                var carsCategories = ItemsService.getCarsCategories();
+                carsCategories.forEach(function(cat) {
+                    if(cat.addCarPanelAvailable) {
+                        cat.addCarPanelAvailable = false;    
+                        ItemsService.updateCarCategory(cat);
+                    }
+                });
+                category.addCarPanelAvailable = true;
+            }
+            ItemsService.updateCarCategory(category);
+        }
+        
+        $scope.editCar = function(id, carId) {
+            if($scope.carEdit.id == id && $scope.carEdit.carId == carId) {
+                return true;
+            } else {
+                return false;
+            }
+        }
+
+        $scope.showCarDetails = function(id, carId) {
+            if($scope.carDetails.id == id && $scope.carDetails.carId == carId) {
+                if($scope.editCar(id, carId)) {
+                    return false;
+                } else {
+                    return true;
+                }
+            } else {
+                return false;
+            }
+        }   
+
+        $scope.removeCar = function(id, carId) {
+            $scope.carsCategories[id].cars.splice(carId, 1);
+            $scope.updateCarCategory(id);
+        }     
+
+        $scope.updateCategoryEditVisibility = function(id) {
+            if($scope.carCategoryEdit.id == -1000) {
+                $scope.carCategoryEdit.id = id;
+            } else {
+                $scope.carCategoryEdit.id = -1000;
+                $scope.updateCarCategory(id);
+            }
+        }   
+
+        $scope.showEditCategory = function(categoryId) {
+            if($scope.carCategoryEdit.id == categoryId) {
+                return true;
+            } else {
+                return false;
+            }
+
+        }  
+
+
+    })
